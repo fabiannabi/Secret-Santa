e789d3b4b659ea8979556a3624c12110c1656767
fabian <fabiannabi@yahoo.com.mx>
2019-12-03 21:03:58 -0600

    dist files

--- dist/functionality.e7055a7a.js
+++ dist/functionality.e7055a7a.js
@@ -123,6 +123,20 @@ var GetName = document.querySelector("#participant-name");
 var GetEmail = document.querySelector("#participant-email");
 var display = document.querySelector("#participants-list");
 var status = document.querySelector(".status-message");
+var ResetList = document.querySelector(".Reset");
+var StartButton = document.querySelector(".Start"); //check if theres Data in localstorage so we can keep track of previous changes between page reloads
+
+if (localStorage.getItem("Data")) {
+  //if there is, use it for Data
+  Data = JSON.parse(localStorage.getItem("Data"));
+} else {
+  //if not initialize data as an empty object
+  Data = {
+    people: []
+  };
+}
+
+console.log(Data);
 AddParticipant.addEventListener("submit", function (event) {
   //submit pide peticion a los servidores cuando el boton es presionado, para eso se previene con ..
   event.preventDefault(); //clear Error Message
@@ -134,24 +148,89 @@ AddParticipant.addEventListener("submit", function (event) {
     status.className = "Error";
     status.innerText = "Please Complete the Form";
     return;
-  } //add person to list
+  } // add to the objetc in the format of
+  //DATA = {
+  //people : [
+  //{name : name, email: email} ]
 
+
+  Data.people.push({
+    name: GetName.value,
+    email: GetEmail.value
+  }); //add person to list
 
   var node = document.createElement("Li");
-  node.innerText = GetName.value;
+  var namenode = document.createElement("span");
+  var emailnode = document.createElement("span");
+  namenode.className = "nameNode";
+  emailnode.className = "emailNode";
+  namenode.innerText = "Name : " + GetName.value;
+  emailnode.innerText = "EMail : " + GetEmail.value;
+  node.appendChild(namenode);
+  node.appendChild(emailnode);
   display.appendChild(node); //clears input fields
 
   GetName.value = "";
   GetEmail.value = ""; //save data
+  //para guardar objetos en storage, hacerlos en strings
 
   localStorage.setItem("participants", display.innerHTML);
+  localStorage.setItem("Data", JSON.stringify(Data)); //para recuperarlos y obtener el objeto original es con parse.
+  //console.log(JSON.parse(localStorage.getItem("Data")));
+}); //reset list
+
+AddParticipant.addEventListener("reset", function (event) {
+  event.preventDefault(); //clears input fields
+
+  GetName.value = "";
+  GetEmail.value = "";
+  localStorage.setItem("participants", "");
+  localStorage.setItem("Data", "");
+  display.innerHTML = "";
 }); // check for saved data
 
-var SavedData = localStorage.getItem("participants");
-console.log(SavedData); //if there are any saved items update list
+var SavedData = localStorage.getItem("participants"); //if there are any saved items update list
 
 if (SavedData) {
   display.innerHTML = SavedData;
+} // make loop
+
+
+var initialPeople;
+var shuffledPeople;
+
+var secretSanta = function secretSanta() {
+  var initialPeople = JSON.parse(localStorage.getItem("Data")).people;
+  console.log("initialPeople");
+  console.log(initialPeople);
+  shuffledPeople = initialPeople.map(function (people) {
+    return people;
+  });
+  console.log("shuffledPeople");
+  console.log(shuffledPeople); //shuffle the array
+
+  shuffle(initialPeople); //clone the array
+
+  /*-------------------------------
+  ERROR
+  ----------------------------------*/
+  //shuffledClone = initialPeople.slice();
+  //RecivingPerson = shuffledClone.unshift(shuffledClone.pop());
+};
+
+StartButton.addEventListener("click", function () {
+  secretSanta(); // for (let i = 0; i < shuffledClone.length; i++) {}
+});
+
+function shuffle(a) {
+  for (var i = a.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var _ref = [a[j], a[i]];
+    a[i] = _ref[0];
+    a[j] = _ref[1];
+  }
+
+  return a;
 }
 },{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
@@ -181,7 +260,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55533" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49549" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
